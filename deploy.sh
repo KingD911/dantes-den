233bf717fb52d0d33ab64f1ee516a1c21cb8b10f
Darryll Siame <darrylltsiame@gmail.com>
2022-08-13 13:00:33 +0200

    last commit

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:KingD911/dantes-den.git main:gh-pages
+git push -f https://github.com/KingD911/dantes-den.git
 
 cd -
