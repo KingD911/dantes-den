4c81d86253bd45504a3148de4332054422e8347f
Darryll Siame <darrylltsiame@gmail.com>
2022-08-13 12:31:07 +0200

    first commit

---
+++ src/components/TheHome.vue
@@ -0,0 +1,20 @@
+<template>
+<header class="invite">
+    <div class="container">
+        <div class="box">
+            hi
+        </div>
+    </div>
+</header>
+</template>
+
+<style scoped>
+.box {
+    background: blue;
+    height: 190px;
+    width: 50rem;
+}
+.invite .container {
+    height: 2000px;
+}
+</style>
